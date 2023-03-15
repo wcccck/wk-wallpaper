b8d912d8f488d531424ca7e0dc760adf0edf7b39
s1mplest <wksimple@163.com>
2023-03-15 13:13:22 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { defineComponent } from "vue"
+export default defineComponent({
+  setup(props, ctx) {
+      return ()=>{
+        return <div>
+          i am app
+        </div>
+      }
+  },
+})
